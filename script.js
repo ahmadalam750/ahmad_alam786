5faa865f0a1050b033c70300f346c1be345ffabe
ahmadalam750 <2009ahmadalam786@gmail.com>
2025-06-08 16:52:10 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function palindromHandler(){
+    console.log("test")
+    var userName = document.getElementById("userValue").value
+    var result = document.getElementById("result")
+    var addValue = ""
+    var isSearch = false
+
+    for(var i=userName.length-1; i>=0;i-- ){
+         addValue = addValue + userName[i]
+         if(addValue == userName){
+            isSearch = true
+            
+         }
+    }
+
+    if(isSearch){
+        console.log("Palindrom")
+        result.style.display = "block"
+        result.innerHTML = "It is Palindrom"
+         console.log(userValue="")
+    }
+    else{
+        console.log("Not Palindrom")
+        result.style.display = "block"
+        result.innerHTML = "It is not Palindrom"
+    }
+}
