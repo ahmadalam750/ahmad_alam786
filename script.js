e985ac5265f98c57a34fa3d89f8e4db28427a14f
ahmadalam750 <2009ahmadalam786@gmail.com>
2025-06-14 01:35:30 +0500

    script.js

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
-function palindromHandler(){
-    console.log("test")
-    var userName = document.getElementById("userValue").value
-    var result = document.getElementById("result")
-    var addValue = ""
-    var isSearch = false
-
-    for(var i=userName.length-1; i>=0;i-- ){
-         addValue = addValue + userName[i]
-         if(addValue == userName){
-            isSearch = true
-            break
-         }
-    }
-
-    if(isSearch){
-        console.log("Palindrom")
-        result.style.display = "block"
-        result.innerHTML = "It is Palindrom"
-         console.log(userValue="")
-    }
-    else{
-        console.log("Not Palindrom")
-        result.style.display = "block"
-        result.innerHTML = "It is not Palindrom"
-    }
-}
+function getRandomHexColor() {
+    const hex = Math.floor(Math.random() * 0xffffff).toString(16);
+    return `#${hex.padStart(6, '0')}`;
+  }
+  
+  function generateColor() {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+    document.getElementById("colorCode").textContent = color;
+  }
+  
+  
+  
+  // Set initial color when page loads
+  window.onload = generateColor;
+  
